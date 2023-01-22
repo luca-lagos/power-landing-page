1138fa28b822247c853b281d968f9b13d989a5c8
Luca Lagos <lagos.herrera.luca@gmail.com>
2023-01-22 03:57:55 -0300

    Upload first structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/** SEGUIR POR MIN 26 */
+
+const Header = () => {
+  return (
+    <header className='flex w-full items-center p-4'>
+        <div className='bg-red-300 w-1/6'><h2>Power</h2></div>
+        <nav className='bg-red-600 flex-1'><h2>Nav</h2></nav>
+    </header>
+  )
+}
+
+export default Header
